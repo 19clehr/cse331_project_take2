009e3551176a87c785124abfd4dec7c119f47760
cjlehr <cjlehr@buffalo.edu>
2023-04-03 21:07:31 -0400

    is this what it asks for?

--- Solution.py
+++ Solution.py
@@ -20,6 +20,10 @@ class Solution:
         paths, bandwidths, priorities = {}, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
-        paths = bfs_path(self.graph,self.isp,self.info)
-        print(paths)
+         paths, bandwidths, priorities = {}, {}, {}
+        client_list = [-1]*len(self.graph)
+        for client in self.graph:
+            if client != self.isp:
+                client_list.append(client)
+        paths = bfs_path(self.graph,self.isp,client_list)
         return (paths, bandwidths, priorities)
