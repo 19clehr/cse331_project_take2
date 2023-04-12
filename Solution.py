26c24003e2c0a56f089f10d17cd8098a8bb8f5db
Sayaf1231 <112524882+Sayaf1231@users.noreply.github.com>
2023-04-12 14:29:07 -0400

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -12,6 +12,20 @@ class Solution:
         self.graph = graph
         self.info = info
 
+        #buffer is < infinity
+        #theashold => 1
+        #find a path that genarates the most revenue
+        #is not the bfs path (so it seems)(rec)
+        #bandwidth is the amount of packet it can send at a tick
+        #treshold is the max wait time (so if tick > treshold == no payment) (i belive thats what alpha is gotta check again tbh)
+
+        #isp = start node
+        #client_list = info["list_clients"]
+        #bandwidths = info["bandwidths"]
+        #alphas(threshold) = info["alphas"]
+        #payments = info["payments"]   not sure if we need to use this
+        #basically create the shortest path function with these constraints
+        
     def output_paths(self):
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
@@ -20,10 +34,13 @@ class Solution:
         paths, bandwidths, priorities = {}, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
-         paths, bandwidths, priorities = {}, {}, {}
-        client_list = [-1]*len(self.graph)
-        for client in self.graph:
-            if client != self.isp:
-                client_list.append(client)
-        paths = bfs_path(self.graph,self.isp,client_list)
+        
+        
+        
+        
+
+        paths = bfs_path(self.graph, self.isp, self.info["list_clients"])
+        bandwidths.update(self.info["bandwidths"])
+
         return (paths, bandwidths, priorities)
+
