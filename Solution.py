a0284f2612e0a38a86c03c2168448746e23e7750
Sayaf1231 <112524882+Sayaf1231@users.noreply.github.com>
2023-05-05 16:17:10 -0400

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -13,87 +13,110 @@ class Solution:
         self.info = info
 
     def output_paths(self):
-        paths, bandwidths, priorities = {}, {}, {}
+        print(self.info["rho1"])
+        print(self.info["rho2"])
+        print(self.info["is_fcc"])
+        paths = {}
+        bandwidths = self.info["bandwidths"].copy()
+        priorities = {}  # If priorities are needed, populate them accordingly
+
         def bfs_path(graph, isp, list_clients):
-        
             myQueue = []
-            myQueue.append((isp,[]))
-            visited = []
-            tick = 0
-            while len(list_clients) != 0:
-                #print(len(list_clients))
+            myQueue.append((isp, []))
+            visited = set()
+
+            while myQueue and list_clients:
                 currentNode, pathSoFar = myQueue.pop(0)
-                if currentNode not in visited:
-                    visited.append(currentNode)
-                else:
+
+                if currentNode in visited:
                     continue
 
+                visited.add(currentNode)
                 newPathSoFar = pathSoFar.copy()
                 newPathSoFar.append(currentNode)
+
                 if currentNode in list_clients:
                     list_clients.remove(currentNode)
                     paths[currentNode] = newPathSoFar
-                
+
                 for neighbor in graph[currentNode]:
                     if neighbor not in visited:
-                        myQueue.append((neighbor,newPathSoFar))
-
+                        myQueue.append((neighbor, newPathSoFar))
+            
             return paths
+
         bfsclients = self.info["list_clients"].copy()
-        paths = bfs_path(self.graph,self.isp,bfsclients)
+        bfs_path(self.graph, self.isp, bfsclients)
 
-
-        currentlyFilled = [0]*len(self.graph)
-        ticks = [1]*len(self.graph)
+        currentlyFilled = [0] * len(self.graph)
+        ticks = [1] * len(self.graph)
         pathsFinished = 0
         nodes = self.info["list_clients"].copy()
         paths2 = paths.copy()
+
         while len(nodes) != 0:
             nodesToMove = nodes.copy()
             visitedNodes = []
+
             while len(nodesToMove) != 0:
                 client = nodesToMove.pop(0)
-                print(client)
+
                 if client in visitedNodes:
-                    for i in range(0,len(currentlyFilled)-1):
-                        currentlyFilled[i] = currentlyFilled[i] - self.info["bandwidths"][i]
+                    for i in range(len(currentlyFilled)):
+                        currentlyFilled[i] -= self.info["bandwidths"][i]
                         if currentlyFilled[i] < 0:
                             currentlyFilled[i] = 0
                 else:
                     visitedNodes.append(client)
-                
-                path = paths2[client]
-                print(path)
-                nodeToVist = path.pop(1)
 
-                if nodeToVist == client:
-                    print("made it")
-                    print(client)
+                path = paths2[client]
+                nodeToVisit = path.pop(1)
+
+                if nodeToVisit == client:
                     nodes.remove(client)
                     pathsFinished += 1
                     ticks[client] += 1
-                print(len(currentlyFilled))
-                print(nodeToVist)
-                fill = currentlyFilled[nodeToVist]
+
+                #print(ticks)
+
+                alpha_complaint=0
+                band_cost = self.info["cost_bandwidth"]
+                beta_complaint = 0
+
+                alpha = self.info["alphas"][client]
+                beta = self.info["betas"][client]
+
+                threshold_fcc = self.info["rho2"]
+
+                #idk if we wanna increase band for everyone who complains
+                if (ticks[client] > beta):
+                    if(client in self.info["is_fcc"]):
+                        beta_complaint += 1
+
+                if(beta_complaint > threshold_fcc * len(self.info["is_fcc"])):
+                    bandwidths[client] += 1
+                    band_cost += self.info["cost_bandwidth"]
+
+                #i assume we have to increase everytime there is a tick>unsubscribe thres
+                if (ticks[client] > alpha):
+                    if(client in self.info["is_fcc"]):
+                        bandwidths[client] += 1
+                        band_cost += self.info["cost_bandwidth"]
+                        alpha_complaint += 1
                 
-                if fill < self.info["bandwidths"][nodeToVist]:
-                    print("packet moved")
-                    currentlyFilled[nodeToVist] += 1
+
+                fill = currentlyFilled[nodeToVisit]
+
+                if fill < self.info["bandwidths"][nodeToVisit]:
+                    currentlyFilled[nodeToVisit] += 1
                 else:
                     nodesToMove.append(client)
-                    paths2[client].insert(0,nodeToVist)
-                    ticks[client] += 1
-                
+                    paths2[client].insert(0, nodeToVisit)
 
                 
-            
-        print(ticks)        
+                #if(ticks[client] > beta):
+                    
 
-        """
-        This method must be filled in by you. You may add other methods and subclasses as you see fit,
-        but they must remain within the Solution class.
-        """
+        
 
-        # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
-        # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
-        return (paths, self.info["bandwidths"], priorities)
+        return paths2, self.info["bandwidths"], priorities
